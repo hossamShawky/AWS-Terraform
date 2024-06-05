a0df5c1accf909decb14a3270d9dcc8353de9566
Hossam Shawky <hossamshawky288@gmail.com>
2024-06-05 23:31:48 +0300

    provision networks

---
+++ deploy-simple-app-ecs-fargate/networks.tf
@@ -0,0 +1,43 @@
+resource "aws_vpc" "vpc" {
+  cidr_block = var.cidr_block
+  tags = {
+    "Name" = "${var.project}-vpc"
+  }
+}
+
+resource "aws_subnet" "subnet" {
+
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+  tags = {
+    "Name" = "${var.project}-subnet"
+  }
+}
+
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    "Name" = "${var.project}-igw"
+  }
+}
+
+
+
+resource "aws_route_table" "aws_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    gateway_id = aws_internet_gateway.igw.id
+    cidr_block = "0.0.0.0/0"
+  }
+  tags = {
+    "Name" = "${var.project}-routetable"
+  }
+}
+
+resource "aws_route_table_association" "aws_route_table_association" {
+  route_table_id = aws_route_table.aws_route_table.id
+  subnet_id      = aws_subnet.subnet.id
+}
---
+++ deploy-simple-app-ecs-fargate/varaibles.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  default = "us-east-1"
+}
+variable "project" {
+  default = "demo-fargate"
+}
+
+
+variable "cidr_block" {
+  default = "10.0.0.0/16"
+}
