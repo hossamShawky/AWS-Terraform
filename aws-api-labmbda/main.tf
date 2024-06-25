6b2b8acab7d1ca7f0b835265f80896e0947c9a48
Hossam Shawky <hossamshawky288@gmail.com>
2024-06-25 17:10:14 +0300

    Methods

--- aws-api-labmbda/main.tf
+++ aws-api-labmbda/main.tf
@@ -106,31 +106,7 @@ resource "aws_api_gateway_resource" "items" {
   path_part   = "resource"
 }
 
-resource "aws_api_gateway_method" "get_items" {
-  rest_api_id   = aws_api_gateway_rest_api.my_api.id
-  resource_id   = aws_api_gateway_resource.items.id
-  http_method   = "GET"
-  authorization = "NONE"
-}
 
-resource "aws_api_gateway_integration" "lambda_get_items" {
-  rest_api_id             = aws_api_gateway_rest_api.my_api.id
-  resource_id             = aws_api_gateway_resource.items.id
-  http_method             = aws_api_gateway_method.get_items.http_method
-  integration_http_method = "POST"
-
-  type = "AWS_PROXY"
-  uri  = aws_lambda_function.my_lambda.invoke_arn
-}
-
-
-resource "aws_api_gateway_deployment" "my_deployment" {
-  depends_on = [
-    aws_api_gateway_integration.lambda_get_items
-  ]
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
-  stage_name  = "prod"
-}
 #Grant API Access To invoke Lambda
 
 resource "aws_lambda_permission" "api_gateway" {
--- aws-api-labmbda/methods.tf
+++ aws-api-labmbda/methods.tf
@@ -1,4 +1,26 @@
+#GET ITEMS
+resource "aws_api_gateway_method" "get_items" {
+  rest_api_id   = aws_api_gateway_rest_api.my_api.id
+  resource_id   = aws_api_gateway_resource.items.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
 
+resource "aws_api_gateway_integration" "lambda_get_items" {
+  rest_api_id             = aws_api_gateway_rest_api.my_api.id
+  resource_id             = aws_api_gateway_resource.items.id
+  http_method             = aws_api_gateway_method.get_items.http_method
+  integration_http_method = "POST"
+
+  type = "AWS_PROXY"
+  uri  = aws_lambda_function.my_lambda.invoke_arn
+}
+
+
+
+
+
+# POST METHOD --> Insert
 resource "aws_api_gateway_method" "post_item" {
   rest_api_id   = aws_api_gateway_rest_api.my_api.id
   resource_id   = aws_api_gateway_resource.items.id
@@ -13,4 +35,37 @@ resource "aws_api_gateway_integration" "lambda_post_item" {
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.my_lambda.invoke_arn
+}
+
+
+
+# DELETE ITEM
+resource "aws_api_gateway_method" "delete_item" {
+  rest_api_id   = aws_api_gateway_rest_api.my_api.id
+  resource_id   = aws_api_gateway_resource.items.id
+  http_method   = "DELETE"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_delete_item" {
+  rest_api_id             = aws_api_gateway_rest_api.my_api.id
+  resource_id             = aws_api_gateway_resource.items.id
+  http_method             = aws_api_gateway_method.delete_item.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.my_lambda.invoke_arn
+}
+
+
+
+
+#DEPLOYEMNT
+
+resource "aws_api_gateway_deployment" "my_deployment" {
+  depends_on = [
+    aws_api_gateway_integration.lambda_get_items,
+    aws_api_gateway_integration.lambda_delete_item
+  ]
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  stage_name  = "prod"
 }
