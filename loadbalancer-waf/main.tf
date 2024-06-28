f245ffdadb1cbba8a3fde96d966a5d307ffd9455
Hossam Shawky <hossamshawky288@gmail.com>
2024-06-28 11:35:50 +0300

    Subnets

--- loadbalancer-waf/main.tf
+++ loadbalancer-waf/main.tf
@@ -5,3 +5,17 @@ module "VPC" {
   cidr_block = "12.0.0.0/16"
   project    = var.project
 }
+
+#@Subnets
+
+module "Public_Subnets" {
+  source             = "./modules/Subnets"
+  vpc_id             = module.VPC.vpc_id
+  igw                = module.VPC.igw
+  project            = var.project
+  destination        = "0.0.0.0/0"
+  cidrs              = ["12.0.1.0/24", "12.0.2.0/24"]
+  availability_zones = ["us-east-1a", "us-east-1b"]
+  type               = "public"
+
+}
