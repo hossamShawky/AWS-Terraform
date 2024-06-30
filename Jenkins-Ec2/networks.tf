3f20a4db4a4d7a6c47e05dcc08dd1b3cf81f7dd9
Hossam Shawky <hossamshawky288@gmail.com>
2024-06-30 12:37:11 +0300

    Route Table

--- Jenkins-Ec2/networks.tf
+++ Jenkins-Ec2/networks.tf
@@ -22,3 +22,19 @@ resource "aws_subnet" "jenkins_subnet" {
   }
   map_public_ip_on_launch = true
 }
+
+resource "aws_route_table" "jenkins_route_table" {
+  vpc_id = aws_vpc.jenkins_vpc.id
+  route {
+    gateway_id = aws_internet_gateway.jenkins_igw.id
+    cidr_block = "0.0.0.0/0"
+  }
+  tags = {
+    "Name" = "${var.project}-public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "jenkins_route_table_association" {
+  route_table_id = aws_route_table.jenkins_route_table.id
+  subnet_id      = aws_subnet.jenkins_subnet.id
+}
